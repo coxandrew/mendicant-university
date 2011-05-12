547b03d0847246ba76cbe312876e22fb94719d6d
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-12 15:57:19 -0400

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MendicantUniversity::Application.config.secret_token = '3e9afeea3cd815e7b732825e9c90bb3bcc298b538f3d449e7cce929997513bb5239a17a6d0de24e28adb73db900fc919b62f1eeb9d015beae4cc4203c8cae1ba'
