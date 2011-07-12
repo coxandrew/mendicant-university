21a28408a2a2e01850ad5e1ad8ef288b301ea17e
Andrew Cox <drew.cox@gmail.com>
2011-07-12 18:39:39 -0400

    Added basic auth for heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "mu" && password == "unicorns"
+    end
+  end
 end
